10f236fbb756d63940c407ad3cfcb240935bddea
Luke Phillips <luke.w.phillips@hotmail.co.uk>
2023-04-29 14:22:12 +1000

    .env

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Home from "./pages/Home";
 import MyWork from "./pages/MyWork";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router history={browserHistory} basename={"/Hawthorn"}>
+    <Router history={browserHistory} basename={"/"}>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/work" element={<MyWork />} />
