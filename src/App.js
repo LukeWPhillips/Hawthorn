1978d0131705a81650df291ff5bb2b1e46abdc76
Luke Phillips <luke.w.phillips@hotmail.co.uk>
2023-04-29 15:30:24 +1000

    .env

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from "./pages/Home";
 import MyWork from "./pages/MyWork";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
